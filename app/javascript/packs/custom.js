bdf36c09e91ea0af24e3f8653bcce4d4f36bb377
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2021-01-11 19:09:37 +0500

    finalized formula approach

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -5,11 +5,11 @@ $( document ).on('turbolinks:load', function() {
     const question = $(this).data('question');
     const answer = $(this).data('answer');
     const order = $(this).data('order');
-    status = evaluate(`${answer.value} == 4`);
+    status = evaluate(order.formula.replace(`${question.variable}` , `${answer.value}`));
     const clickStatus = $(`.order-msg[data-source=${question.id}]`).text().length;
     if(clickStatus.toString() === '0' ){
       $(`.order-msg[data-source=${question.id}]`).show();
-      $(`.order-msg[data-source=${question.id}]`).append(`${order}`);
+      $(`.order-msg[data-source=${question.id}]`).append(`${order.name}`);
       status === 'true' ? $(`.order-msg[data-source=${question.id}]`).addClass('alert-success') : $(`.order-msg[data-source=${question.id}]`).addClass('alert-danger');
     }
   });
