331d007bc812f6c4c8eef5cd7c774068f5531222
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2021-01-11 19:58:26 +0500

    handled an issue with order name display

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -4,12 +4,13 @@ $( document ).on('turbolinks:load', function() {
   $('.answer-option').on("click", function () {
     const question = $(this).data('question');
     const answer = $(this).data('answer');
-    const order = $(this).data('order');
-    status = evaluate(order.formula.replace(`${question.variable}` , `${answer.value}`));
+    const orderFirst = $(this).data('orderfirst');
+    const orderSecond = $(this).data('ordersecond');
+    status = evaluate(orderFirst.formula.replace(`${question.variable}` , `${answer.value}`));
     const clickStatus = $(`.order-msg[data-source=${question.id}]`).text().length;
     if(clickStatus.toString() === '0' ){
       $(`.order-msg[data-source=${question.id}]`).show();
-      $(`.order-msg[data-source=${question.id}]`).append(`${order.name}`);
+      status === 'true' ? $(`.order-msg[data-source=${question.id}]`).append(`${orderFirst.name}`) : $(`.order-msg[data-source=${question.id}]`).append(`${orderSecond.name}`);
       status === 'true' ? $(`.order-msg[data-source=${question.id}]`).addClass('alert-success') : $(`.order-msg[data-source=${question.id}]`).addClass('alert-danger');
     }
   });
