34b5b1d5690be75b3645ce437a175e59b452b7c0
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2021-01-11 18:40:02 +0500

    implemented the evaluation and alert display with forms and show styling

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,7 +1,16 @@
+import { evaluate } from 'mathjs';
 
 $( document ).on('turbolinks:load', function() {
   $('.answer-option').on("click", function () {
-    const questionId = $(this).data('question');
-    $(`.order-msg[data-source=${questionId}]`).show();
+    const question = $(this).data('question');
+    const answer = $(this).data('answer');
+    const order = $(this).data('order');
+    status = evaluate(`${answer.value} == 4`);
+    const clickStatus = $(`.order-msg[data-source=${question.id}]`).text().length;
+    if(clickStatus.toString() === '0' ){
+      $(`.order-msg[data-source=${question.id}]`).show();
+      $(`.order-msg[data-source=${question.id}]`).append(`${order}`);
+      status === 'true' ? $(`.order-msg[data-source=${question.id}]`).addClass('alert-success') : $(`.order-msg[data-source=${question.id}]`).addClass('alert-danger');
+    }
   });
 });
