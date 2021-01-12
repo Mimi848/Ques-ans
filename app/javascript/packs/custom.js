c1f1fde4c12b28ab0e744ed8953fd743df583f6e
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2021-01-12 21:19:08 +0500

    added radio button disable after checked option

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -8,10 +8,11 @@ $( document ).on('turbolinks:load', function() {
     const orderSecond = $(this).data('ordersecond');
     status = evaluate(orderFirst.formula.replace(`${question.variable}` , `${answer.value}`));
     const clickStatus = $(`.order-msg[data-source=${question.id}]`).text().length;
-    if(clickStatus.toString() === '0' ){
+    if( clickStatus.toString() === '0' ){
       $(`.order-msg[data-source=${question.id}]`).show();
       status === 'true' ? $(`.order-msg[data-source=${question.id}]`).append(`${orderFirst.name}`) : $(`.order-msg[data-source=${question.id}]`).append(`${orderSecond.name}`);
       status === 'true' ? $(`.order-msg[data-source=${question.id}]`).addClass('alert-success') : $(`.order-msg[data-source=${question.id}]`).addClass('alert-danger');
     }
+    else $(this).attr('disabled', true);
   });
 });
