a93a0869079efbea1faa26b11c897b7f7c83bab7
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2021-01-15 15:31:12 +0500

    made changes for the postgresql integration

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_01_07_131749) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "text"
-    t.integer "question_id"
+    t.bigint "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "value"
